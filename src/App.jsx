cddf1d14d55ac4c34fa2b23171c5a5d583be6b9d
FedeOrefici <federicoaorefici@gmail.com>
2022-08-21 16:07:57 +0200

    firstUpdate

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import Header from "./components/Header"
+import Formulario from "./components/Formulario"
+import {useState, useEffect} from 'react'
+import ListadoPacientes from "./components/ListadoPacientes";
+
+function App() {
+
+  const [pacientes,setPacientes] = useState ([]);
+  const [paciente, setPaciente] =useState ({});
+  
+  //eliminar paciente
+  const eliminarPaciente = id => {
+    const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id);
+      setPacientes(pacientesActualizados);
+  }
+
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+
+        <div className="mt-12 md:flex">
+          <Formulario 
+            pacientes = {pacientes}
+            setPacientes = {setPacientes}
+            paciente = {paciente}
+          />
+
+          <ListadoPacientes
+            pacientes={pacientes}
+            setPaciente={setPaciente}
+            eliminarPaciente={eliminarPaciente}
+          />
+
+        </div>
+    </div>
+  )
+}
+
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header () {
+    
+    return (
+        <>
+            <h1 className="font-black text-4xl text-center">Seguimiento pacientes {''}
+                <span className="text-indigo-600">Veterinaria</span>
+            </h1>
+        </>
+    )
+}
+
+
+export default Header;
